b5756324c6b1f48fc64ccae42c17949fa095b3ae
jorgam8 <76073257+jorgam8@users.noreply.github.com>
2021-01-22 15:01:50 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyARTT0283wsZR56vTTHoICNlhLcgOdn5iE",
+    authDomain: "ejemploproj.firebaseapp.com",
+    projectId: "ejemploproj",
+    storageBucket: "ejemploproj.appspot.com",
+    messagingSenderId: "976474973551",
+    appId: "1:976474973551:web:f7ccf5cc10a73ba66f349a"
+    });
